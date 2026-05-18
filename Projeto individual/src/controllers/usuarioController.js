e4f64736a0d1e108a5562c5851d9fdc9a6933246
vitor <vitor.machado@sptech.school>
2026-05-18 14:04:14 -0300

    adicionando titulos aos graficos

--- Projeto individual/src/controllers/usuarioController.js
+++ Projeto individual/src/controllers/usuarioController.js
@@ -3,6 +3,7 @@ var usuarioModel = require("../models/usuarioModel");
 function autenticar(req, res) {
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
+    
 
     if (email == undefined) {
         res.status(400).send("Seu email está undefined!");
